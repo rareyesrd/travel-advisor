c00db7d4768d0ab8911e1fa0774508b64e6ace1b
Rafael Alexander Reyes Pérez <rafaelalexanderr5@gmail.com>
2022-03-24 04:34:46 -0400

    [ADD] list filter

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-import { Autocomplete } from "@react-google-maps/api";
-
 import { AppBar, Toolbar, Typography, InputBase, Box } from "@material-ui/core";
+import { Autocomplete } from "@react-google-maps/api";
 import SearchIcon from "@material-ui/icons/Search";
-import useStyles from "./styles";
+
+import useStyles from "./styles.js";
 
 const Header = () => {
   const classes = useStyles();
@@ -18,7 +18,7 @@ const Header = () => {
           <Typography variant="h6" className={classes.title}>
             Explore new places
           </Typography>
-          <Autocomplete>
+          {/* <Autocomplete>
             <div className={classes.search}>
               <div className={classes.searchIcon}>
                 <SearchIcon />
@@ -31,7 +31,7 @@ const Header = () => {
                 }}
               />
             </div>
-          </Autocomplete>
+          </Autocomplete> */}
         </Box>
       </Toolbar>
     </AppBar>
