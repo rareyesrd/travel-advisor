9ad2dba2eb5c20a2bef4e2e908fd4e12925271a6
Rafael Alexander Reyes Pérez <rafaelalexanderr5@gmail.com>
2022-03-24 04:34:00 -0400

    [ADD] map view connected to GCP

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationOutlinedIcon from "@material-ui/lab";
+import GoogleMapReact from "google-map-react";
+
+import useStyles from "./styles";
+
 const Map = () => {
-  return <h1>Map</h1>;
+  const classes = useStyles();
+  const isMobile = useMediaQuery("(min-width: 600px)");
+
+  const coordinates = { lat: 0, lng: 0 };
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyBFC1t9Wa-NG4qhBliNBLZsVCDqUwZUg1s" }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        option={""}
+        // onChange={""}
+        onChlidClik={""}
+      ></GoogleMapReact>
+    </div>
+  );
 };
 
 export default Map;
